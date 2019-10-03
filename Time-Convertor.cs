6596616d44eef32715968da97aca843f188811ea
MiroBoyBG <43450825+MiroBoyBG@users.noreply.github.com>
2019-10-03 12:07:01 -0700

    Add year and month fields

--- Time-Convertor.cs
+++ Time-Convertor.cs
@@ -5,14 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace Time_Convertor
+namespace first_project
 {
-    class Program
+     class Program
     {
+       public static long  year , month , week , day , hour , min , sec , time = 0 ;
+         static void Options()
+            {
+              if(sec > 10 && min > 10 && hour > 10)
+            {
+                Console.WriteLine("   time =   " + year + " y : " + month + " m : " + week + " w : " + day + " d : " + hour + " h : " + min + " min : " + sec + " s" );
+            }
+            if( sec < 10 && min > 10 && hour > 10)
+            {
+                Console.WriteLine("   time =  " + year + " y : " + month + " m : " + week + " w : " + day + " d : " + hour + " h : " + min + " min : 0" + sec + " s" );
+            }
+            if( min < 10 && sec > 10 && hour > 10)
+            {
+                Console.WriteLine("   time =  " + year + " y : " + month + " m : " + week + " w : " + day + " d : " + hour + " h :  0" + min + " min : " + sec + " s" );
+            }
+            if( sec < 10 && min < 10 && hour > 10)
+            {
+                Console.WriteLine("   time =  " + year + " y : " + month + " m : " + week + " w : " + day + " d : " + hour + " h :  0" + min + " min :  0" + sec + " s" );
+            }
+            if( hour < 10 && min > 10 && sec > 10)
+            {
+                Console.WriteLine("   time =  " + year + " y : " + month + " m : " + week + " w : "  + day + " d :  0"+ hour + " h : " + min + " min : " + sec + " s" );
+            }
+            if( hour < 10 && sec < 10 && min > 10)
+            {
+                Console.WriteLine("   time =  " + year + " y : " + month + " m : " + week + " w : " + day + " d :  0" + hour + " h : " + min + " min :  0" + sec + " s" );
+            }
+            if( hour < 10 && min < 10 && sec > 10)
+            {
+                Console.WriteLine("   time =  " + year + " y : " + month + " m : " + week + " w : " + day + " d :  0" + hour + " h :  0" + min + " min : " + sec + " s" );
+            }
+            if( hour < 10 && min < 10 && sec < 10)
+            {
+                Console.WriteLine("   time =  " + year + " y : " + month + " m : " + week + " w : " + day + " d :  0" + hour + " h :  0" + min + " min :  0" + sec + " s" );
+            }
+
+            }
         static void Main()
         {
             A:
-              long week , day , hour , min , sec , time = 0 ;
               Console.ForegroundColor = ConsoleColor.Cyan;
               Console.WriteLine("Insert the amount of time (in seconds) you want !");
               try
@@ -31,45 +67,15 @@ namespace Time_Convertor
               {
                 time = -time;
               }
-
-              week = time / 604800;
-              day = (time % 604800) / 86400 ; 
-              hour = ((time % 604800) % 86400) / 3600;
-              min = (((time % 604800) % 86400) % 3600) / 60;
-             sec = (((time % 604800) % 86400) % 3600) % 60;
+              year = time / 31104000;
+              month = (time % 31104000) / 2592000;
+              week = ((time % 31104000) % 2592000) / 604800;
+              day = ((time % 31104000) % 2592000) % 604800 / 86400 ; 
+              hour = (((time % 31104000) % 2592000) % 604800 % 86400) / 3600;
+              min = ((((time % 31104000) % 2592000) % 604800 % 86400) % 3600) / 60;
+             sec = ((((time % 31104000) % 2592000 ) % 604800 % 86400) % 3600) % 60;
              Console.ForegroundColor = ConsoleColor.Green;
-            if(sec > 10 && min > 10 && hour > 10)
-            {
-                Console.WriteLine("   time =   " + week + " w : " + day + " d : " + hour + " h : " + min + " m : " + sec + " s" );
-            }
-            if( sec < 10 && min > 10 && hour > 10)
-            {
-                Console.WriteLine("   time =  " + week + " w : " + day + " d : " + hour + " h : " + min + " m : 0" + sec + " s" );
-            }
-            if( min < 10 && sec > 10 && hour > 10)
-            {
-                Console.WriteLine("   time =  " + week + " w : " + day + " d : " + hour + " h :  0" + min + " m : " + sec + " s" );
-            }
-            if( sec < 10 && min < 10 && hour > 10)
-            {
-                Console.WriteLine("   time =  " + week + " w : " + day + " d : " + hour + " h :  0" + min + " m :  0" + sec + " s" );
-            }
-            if( hour < 10 && min > 10 && sec > 10)
-            {
-                Console.WriteLine("   time =  " + week + " w : "  + day + " d :  0"+ hour + " h : " + min + " m : " + sec + " s" );
-            }
-            if( hour < 10 && sec < 10 && min > 10)
-            {
-                Console.WriteLine("   time =  " + week + " w : " + day + " d :  0" + hour + " h : " + min + " m :  0" + sec + " s" );
-            }
-            if( hour < 10 && min < 10 && sec > 10)
-            {
-                Console.WriteLine("   time =  " + week + " w : " + day + " d :  0" + hour + " h :  0" + min + " m : " + sec + " s" );
-            }
-            if( hour < 10 && min < 10 && sec < 10)
-            {
-                Console.WriteLine("   time =  " + week + " w : " + day + " d :  0" + hour + " h :  0" + min + " m :  0" + sec + " s" );
-            }
+              Options();
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("   Press any key to continue ! ");
